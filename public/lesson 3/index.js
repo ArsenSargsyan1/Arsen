ce559a90cddd078c9bb2b6484a2ff90588884a47
ArsenSargsyan1 <arsen.sargsyan.s@tumo.org>
2019-10-09 19:25:37 +0400

    vhhiv

--- public/lesson 3/index.js
+++ public/lesson 3/index.js
@@ -4,9 +4,9 @@ var app = express();
 app.use(express.static("your_project_folder_name"));
 
 app.get("/", function(req, res){
-   res.redirect("your_project_html_name");
+   res.redirect("./");
 });
 
 app.listen(3000, function(){
-   console.log("Example is running on port 3001");
+   console.log("Example is running on port 3000");
 });
