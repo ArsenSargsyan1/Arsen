5777747a995ab937a2889bed4f7dab3e71bbf438
ArsenSargsyan1 <your.name>
2019-10-15 17:15:42 +0400

    lesson4

--- public/lesson4/script.js
+++ public/lesson4/script.js
@@ -1,21 +1,4 @@
-// function windowLoad(){
-//     console.log("Window is loaded");
-// }
-// window.onload = windowLoad;
 
-
-// function preload() {
-//     console.log("Widnow is loaded");
-// }
-// function click(evt) {
-//     console.log(evt.pageX , evt.pageY);
-// }
-// window.onlick = click;
-
-
-// function mousePressed(){
-//     console.log(mouseX , mouseY);
-// }
 
 function main() {
     var socket = io();
